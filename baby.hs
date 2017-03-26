711093bbd2f78ef14ce360b661617633c6bdcc62
Scott Windsor <swindsor@gmail.com>
2017-03-26 15:03:34 -0700

    Chapter 2: baby steps

---
+++ baby.hs
@@ -0,0 +1,13 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
